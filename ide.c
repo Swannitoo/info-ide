3de49f5019125b65a7e7a18d6be7d6cdf468cf01
Swannitoo <swann.arnal@outlook.com>
2026-04-08 11:46:21 +0200

    Ajout d'un ide.c, base pour l'exécutable

---
+++ ide.c
@@ -0,0 +1,23 @@
+#include <ncurses.h>
+#include <string.h>
+
+int main(void) {
+    WINDOW *boite;
+    char *msg= "Texte au centre";
+    int taille= strlen(msg);
+    
+    initscr();
+    while(1) {
+        clear();    // Efface la fenêtre (donc l'ancien message)
+        mvprintw(LINES/2, (COLS / 2) - (taille / 2), msg);
+        refresh();
+        if(getch() != 410)
+            break;
+    }
+    
+    endwin();
+    
+    free(boite);
+    
+    return 0;
+}
