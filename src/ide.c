25e3a21e6d666ce21d957eb083e79d57ecf574de
Swann ARNAL <swann.arnal@outlook.com>
2026-04-09 14:23:19 +0200

    Update ide.c

--- src/ide.c
+++ src/ide.c
@@ -1,1 +1,30 @@
 
+#include <ncurses.h>
+#include <string.
+
+/*
+Valentin (TREVAUX / NOYELLE)
+Swann ARNAL
+Projet Info 2026 : IDE
+github.com/Swannitoo/info-ide
+*/
+int main(void) {
+    WINDOW *boite;
+    char *msg= "Texte au centre";
+    int taille= strlen(msg);
+    
+    initscr();
+    while(1) {
+        clear();    // Efface la fenêtre (donc l'ancien message)
+        mvprintw(LINES/2, (COLS / 2) - (taille / 2), msg);
+        refresh();
+        if(getch() != 410)
+            break;
+    }
+    
+    endwin();
+    
+    free(boite);
+    
+    return 0;
+}
