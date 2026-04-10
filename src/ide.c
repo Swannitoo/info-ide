c46e8d59f191cec7d58344f6851bedbe14d228e3
Swann ARNAL <swann.arnal@outlook.com>
2026-04-10 14:17:09 +0200

    Update ide.c
    j'ai ajouté des tailles aux fenetres

--- src/ide.c
+++ src/ide.c
@@ -1,6 +1,7 @@
 
 #include <ncurses.h>
-#include <string.
+#include <string.>
+#include <panel.h>
 
 /*
 Valentin (TREVAUX / NOYELLE)
@@ -9,7 +10,8 @@ Projet Info 2026 : IDE
 github.com/Swannitoo/info-ide
 */
 int main(void) {
-    WINDOW *boite;
+    WINDOW boite = newwin(100, 300, 5,50);
+	border(0, 0, 0, 0, 0, 0, 0, 0);
     char *msg= "Valentin tais-toi par pitié";
     int taille= strlen(msg);
     
